a72c1b03204c0af58dfbd003b286172325b91a08
rkguru2023 <152018985+rkguru2023@users.noreply.github.com>
2023-11-26 13:34:55 +0800

    typo on listeners

--- main.tf
+++ main.tf
@@ -57,12 +57,12 @@ module "alb" {
   subnets         = module.blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
 
-  http_tcp_listners = [
-   {
-      port                = 80
-      protocol            = "HTTP"
-      targert_group_index = 0
-   }
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
   ]
 
   target_groups = [
