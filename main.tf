d70dca7f34dbcab872bb8fc47e17d427accba9c5
rkguru2023 <152018985+rkguru2023@users.noreply.github.com>
2023-11-26 11:20:48 +0800

    http in security group to port corrected to 80

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "blog"{
 resource "aws_security_group_rule" "blog_http_in"{
   type        ="ingress"
   from_port   = 80
-  to_port     = 0
+  to_port     = 80
   protocol    ="tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
