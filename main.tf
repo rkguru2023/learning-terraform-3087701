05e7477f28b67326db0adf9aa90d96d9ab581476
rkguru2023 <152018985+rkguru2023@users.noreply.github.com>
2023-11-26 12:32:41 +0800

    typo mdoule instead of module

--- main.tf
+++ main.tf
@@ -22,12 +22,13 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids =[moudle.blog_sg.security_group_id]
+  vpc_security_group_ids =[module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
   }
 }
+
 
 module "blog_sg" {
   source      = "terraform-aws-modules/security-group/aws"
