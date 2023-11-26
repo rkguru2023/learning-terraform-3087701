e4e200354e47a7d02ab08289b6c64eba834a5c67
rkguru2023 <152018985+rkguru2023@users.noreply.github.com>
2023-11-26 13:38:32 +0800

    movied the listeners at the end

--- main.tf
+++ main.tf
@@ -57,28 +57,28 @@ module "alb" {
   subnets         = module.blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
 
-  http_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
-  target_groups = [
+  target_groups = {
     {
       name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets ={
+      targets = {
         my_target ={
           target_id  = aws_instance.blog.id
           port = 80
         }
       }
     }
-  ]
+ }
+
+  http_listeners = 
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+
 
   tags = {
     Environment = "Development"
